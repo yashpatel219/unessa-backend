11b92d3734adb13a65b43054d72fcc0ef894dc80
Yash Patel <167839006+yashpatel219@users.noreply.github.com>
2025-08-07 12:59:46 +0530

    Update offer.route.js

--- routes/offer.route.js
+++ routes/offer.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { generateAndSendOffer } from "../controllers/offer.controller.js";
+import { generateAndSendOffer,  getOfferLetter } from "../controllers/offer.controller.js";
 import User from "../models/User.js";
 const router = express.Router();
 
