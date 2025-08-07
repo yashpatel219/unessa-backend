2c109fa08145475eaae2552ff878a9c9f120877e
Yash Patel <167839006+yashpatel219@users.noreply.github.com>
2025-08-07 12:21:19 +0530

    Update offer.route.js

--- routes/offer.route.js
+++ routes/offer.route.js
@@ -4,20 +4,8 @@ import User from "../models/User.js";
 const router = express.Router();
 
 router.post("/generate-offer", generateAndSendOffer);
-router.get("/:userId", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    if (!user || !user.pdf) {
-      return res.status(404).send("Offer not found");
-    }
 
-    res.setHeader("Content-Type", "application/pdf");
-    res.send(user.pdf);
-  } catch (err) {
-    console.error("Failed to fetch offer:", err);
-    res.status(500).send("Server error");
-  }
-});
+
 
 
 export default router;
